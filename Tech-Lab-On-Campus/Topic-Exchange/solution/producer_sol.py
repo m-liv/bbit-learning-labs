b4ac4283f2f77851cdccc51a926028fc94698cdf
m-liv <miya.livingston@icloud.com>
2024-03-19 22:32:46 -0400

    Implemented consume.py and publish.py to read from command line

--- Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
@@ -8,16 +8,17 @@ class mqProducer(mqProducerInterface):
 
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         self.routing_key = routing_key
-        self.channel.exchange_declare(
-            exchange=exchange_name, exchange_type="topic"
-        )
+        self.exchange_name = exchange_name
         self.setupRMQConnection()
+        
 
     def setupRMQConnection(self) -> None:
         con_params = pika.URLParameters(os.environ["AMQP_URL"])
         self.connection = pika.BlockingConnection(parameters=con_params)
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name)
+        self.channel.exchange_declare(
+            exchange=self.exchange_name, exchange_type="topic"
+        )
     
     def publishOrder(self, message: str) -> None:
         self.channel.basic_publish(
@@ -31,6 +32,8 @@ class mqProducer(mqProducerInterface):
         #     routing_key= self.routing_key,
         #     exchange= self.exchange_name,
         # )
+
+    def __del__(self) -> None:
         print("Success!")
         self.channel.close()
         self.connection.close()
